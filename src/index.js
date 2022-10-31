bb981e36173ed21b058f69b627d67471b7d8b481
mugilmalathi <mugilarasan.malathi@gmail.com>
2022-10-31 13:35:26 +0530

    sample backend api creation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const express = require("mongoose");
+const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 require("dotenv").config();
-const connect = require("./configs/db")
+const connect = require("./configs/db");
 
 const PORT = process.env.PORT;
 
