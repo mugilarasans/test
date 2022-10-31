6130b2d076d0a76d53a5dd9aef65f47b7b40ff97
mugilmalathi <mugilarasan.malathi@gmail.com>
2022-10-31 13:45:54 +0530

    sample backend api creation

--- src/index.js
+++ src/index.js
@@ -5,14 +5,16 @@ app.use(cors());
 app.use(express.json());
 require("dotenv").config();
 const connect = require("./configs/db");
-
 const PORT = process.env.PORT;
-
 const sampleController = require("./controller/sample")
 
 app.use("/", sampleController)
 
 app.listen(PORT, async()=>{
-    await connect();
-    console.log("DB is connected...!")
+    try{
+        await connect();
+        console.log("DB is Connected..!");
+    }catch(err){
+        console.log(err);
+    }
 })
